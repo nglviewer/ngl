6e8d4efc96f7ec026be4e432f60b344834cb57c6
Alexander Rose <alexander.rose@weirdbyte.de>
2014-05-13 19:16:39 +0200

    CODE: removed jQuery dependency (replaced with HTML5 and THREE methods)

--- js/ngl.js
+++ js/ngl.js
@@ -3,45 +3,6 @@
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
  */
 
-$.loadImage = function(url) {
-    // from: http://aboutcode.net/2013/01/09/load-images-with-jquery-deferred.html
-    // Define a "worker" function that should eventually resolve or reject the deferred object.
-    var loadImage = function( deferred ) {
-        var image = new Image();
-         
-        // Set up event handlers to know when the image has loaded
-        // or fails to load due to an error or abort.
-        image.onload = loaded;
-        image.onerror = errored; // URL returns 404, etc
-        image.onabort = errored; // IE may call this if user clicks "Stop"
-         
-        // Setting the src property begins loading the image.
-        image.src = url;
-         
-        function loaded() {
-            unbindEvents();
-            // Calling resolve means the image loaded sucessfully and is ready to use.
-            deferred.resolve( image );
-        }
-        function errored() {
-            unbindEvents();
-            // Calling reject means we failed to load the image (e.g. 404, server offline, etc).
-            deferred.reject( image );
-        }
-        function unbindEvents() {
-            // Ensures the event callbacks only get called once.
-            image.onload = null;
-            image.onerror = null;
-            image.onabort = null;
-        }
-    };
-
-    // Create the deferred object that will contain the loaded image.
-    // We don't want callers to have access to the resolve() and reject() methods, 
-    // so convert to "read-only" by calling `promise()`.
-    return $.Deferred( loadImage ).promise();
-};
-
 
 /**
  * [NGL description]
@@ -227,9 +188,9 @@ NGL.Utils = {
 /**
  * Initialize the global NGL object, i.e. get resources
  */
-NGL.init = function () {
+NGL.init = function ( onload ) {
 
-    NGL.initResources();
+    NGL.initResources( onload );
 
     NGL.materialCache = {};
 
@@ -244,34 +205,42 @@ NGL.init = function () {
  * Get resources for the global NGL object
  * @private
  */
-NGL.initResources = function(){
+NGL.initResources = function( onLoad ){
 
-    var deferreds = [];
+    var loadingManager = new THREE.LoadingManager( function(){
 
-    _.each( NGL.Resources, function( v, url ){
-        var d;
-        if( v=="image" ){
-            d = $.loadImage( url ).done( 
-                function( image ){ NGL.Resources[ url ] = image; }
-            );
-        }else{
-            d = $.ajax({
-                url: url,
-                success: function( data ){
-                    if( v=="chunk" ){
-                        NGL.Resources[ url ] = data;
-                    }else{
-                        NGL.Resources[ url ] = data;
-                    }
-                },
-                dataType: "text"
-            });
+        if( onLoad !== undefined ){
+
+            onLoad();
+
         }
-        deferreds.push( d );
+
     });
 
-    $.when.apply( $, deferreds ).then( function() {
-        $( NGL ).triggerHandler( "initialized" );
+    var imageLoader = new THREE.ImageLoader( loadingManager );
+
+    var xhrLoader = new THREE.XHRLoader( loadingManager );
+
+    _.each( NGL.Resources, function( v, url ){
+        
+        if( v=="image" ){
+
+            imageLoader.load( url, function( image ){
+
+                NGL.Resources[ url ] = image;
+
+            });
+            
+        }else{
+
+            xhrLoader.load( url, function( data ){
+
+                NGL.Resources[ url ] = data;
+
+            });
+            
+        }
+        
     });
 
 },
