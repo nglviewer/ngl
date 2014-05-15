7e23590c890c9e70edd765fda1a9edf6f6d2286d
Alexander Rose <alexander.rose@weirdbyte.de>
2014-05-15 22:34:18 +0200

    FIX. indention

--- build.py
+++ build.py
@@ -5,17 +5,17 @@ import os
 
 def main( argv=None ):
 
-	print "building jsdoc..."
-	cmd = ( 
-		'./build/jsdoc/jsdoc '
-		'-d doc/ '
-		'-u data/tutorial/ '
-		'-t build/docstrap/template/ '
-		'-c build/jsdoc.conf.json '
-		'js/ngl.js js/ngl.extra.js js/ngl.wip.js '
-		'README.md'
-	)
-	os.system( cmd )
+    print "building jsdoc..."
+    cmd = ( 
+        './build/jsdoc/jsdoc '
+        '-d doc/ '
+        '-u data/tutorial/ '
+        '-t build/docstrap/template/ '
+        '-c build/jsdoc.conf.json '
+        'js/ngl.js js/ngl.extra.js js/ngl.wip.js '
+        'README.md'
+    )
+    os.system( cmd )
 
 
 
@@ -23,6 +23,6 @@ def main( argv=None ):
 
 
 if __name__ == "__main__":
-	main()
+    main()
 
 
