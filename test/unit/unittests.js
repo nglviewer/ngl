329ee8b267db5a832ebd30d11594131bb908b386
alexander.rose <alexander.rose@weirdbyte.de>
2014-08-06 23:41:21 +0200

    ADD: "not" selection operator, including tests

--- test/unit/unittests.js
+++ test/unit/unittests.js
@@ -3,13 +3,52 @@
 QUnit.module( "selection" );
 
 
+QUnit.test( "selection chain", function( assert ) {
+
+    var sele = ":A";
+
+    var selection = new NGL.Selection( sele );
+
+    var selectionObj = {
+        "operator": undefined,
+        "rules": [
+            { "chainname": "A" }
+        ]
+    };
+
+    assert.deepEqual( selection.selection, selectionObj, "Passed!" );
+
+});
+
+
+QUnit.test( "selection chain resno range", function( assert ) {
+
+    var sele = "1-100:A";
+
+    var selection = new NGL.Selection( sele );
+
+    var selectionObj = {
+        "operator": undefined,
+        "rules": [
+            {
+                "chainname": "A",
+                "resno": [ 1, 100 ]
+            }
+        ]
+    };
+
+    assert.deepEqual( selection.selection, selectionObj, "Passed!" );
+
+});
+
+
 QUnit.test( "selection parens", function( assert ) {
 
     var sele = "10-15 or ( backbone and ( 30-35 or 40-45 ) )";
 
     var selection = new NGL.Selection( sele );
 
-    var selectionObj = { 
+    var selectionObj = {
         "operator": "OR",
         "rules": [
             { "resno": [ 10, 15 ] },
@@ -40,7 +79,7 @@ QUnit.test( "selection no parens", function( assert ) {
 
     var selection = new NGL.Selection( sele );
 
-    var selectionObj = { 
+    var selectionObj = {
         "operator": "OR",
         "rules": [
             { "resno": [ 10, 15 ] },
@@ -66,7 +105,7 @@ QUnit.test( "selection outer parens", function( assert ) {
 
     var selection = new NGL.Selection( sele );
 
-    var selectionObj = { 
+    var selectionObj = {
         "operator": "OR",
         "rules": [
             { "resno": [ 10, 15 ] },
@@ -85,7 +124,7 @@ QUnit.test( "selection parsing error resi", function( assert ) {
 
     var selection = new NGL.Selection( sele );
 
-    var selectionObj = { 
+    var selectionObj = {
         "error": "resi must be an integer"
     };
 
@@ -100,7 +139,7 @@ QUnit.test( "selection parsing error atomname", function( assert ) {
 
     var selection = new NGL.Selection( sele );
 
-    var selectionObj = { 
+    var selectionObj = {
         "error": "atomname must be one to four characters"
     };
 
@@ -115,7 +154,7 @@ QUnit.test( "selection parsing error chain", function( assert ) {
 
     var selection = new NGL.Selection( sele );
 
-    var selectionObj = { 
+    var selectionObj = {
         "error": "chain identifier must be one character"
     };
 
@@ -130,7 +169,7 @@ QUnit.test( "selection parsing error model", function( assert ) {
 
     var selection = new NGL.Selection( sele );
 
-    var selectionObj = { 
+    var selectionObj = {
         "error": "model must be an integer"
     };
 
@@ -145,8 +184,194 @@ QUnit.test( "selection parsing error resi range", function( assert ) {
 
     var selection = new NGL.Selection( sele );
 
-    var selectionObj = { 
+    var selectionObj = {
         "error": "resi range must contain one '-'"
+    };
+
+    assert.deepEqual( selection.selection, selectionObj, "Passed!" );
+
+});
+
+
+QUnit.test( "selection negate simple", function( assert ) {
+
+    var sele = "not 10-15";
+
+    var selection = new NGL.Selection( sele );
+
+    var selectionObj = {
+        "operator": undefined,
+        "negate": true,
+        "rules": [
+            { "resno": [ 10, 15 ] }
+        ]
+    };
+
+    assert.deepEqual( selection.selection, selectionObj, "Passed!" );
+
+});
+
+
+QUnit.test( "selection negate or", function( assert ) {
+
+    var sele = "MET or not 10-15";
+
+    var selection = new NGL.Selection( sele );
+
+    var selectionObj = {
+        "operator": "OR",
+        "rules": [
+            { "resname": "MET" },
+            {
+                "operator": undefined,
+                "negate": true,
+                "rules": [
+                    { "resno": [ 10, 15 ] }
+                ]
+            }
+        ]
+    };
+
+    assert.deepEqual( selection.selection, selectionObj, "Passed!" );
+
+});
+
+
+QUnit.test( "selection negate parens", function( assert ) {
+
+    var sele = "MET or not ( 10-15 )";
+
+    var selection = new NGL.Selection( sele );
+
+    var selectionObj = {
+        "operator": "OR",
+        "rules": [
+            { "resname": "MET" },
+            {
+                "operator": undefined,
+                "negate": true,
+                "rules": [
+                    { "resno": [ 10, 15 ] }
+                ]
+            }
+        ]
+    };
+
+    assert.deepEqual( selection.selection, selectionObj, "Passed!" );
+
+});
+
+
+QUnit.test( "selection negate parens 2", function( assert ) {
+
+    var sele = "MET or not ( 10-15 and 15-20 )";
+
+    var selection = new NGL.Selection( sele );
+
+    var selectionObj = {
+        "operator": "OR",
+        "rules": [
+            { "resname": "MET" },
+            {
+                "operator": "AND",
+                "negate": true,
+                "rules": [
+                    { "resno": [ 10, 15 ] },
+                    { "resno": [ 15, 20 ] }
+                ]
+            }
+        ]
+    };
+
+    assert.deepEqual( selection.selection, selectionObj, "Passed!" );
+
+});
+
+
+QUnit.test( "selection negate parens 3", function( assert ) {
+
+    var sele = "MET or not ( 10-15 and 15-20 ) or GLU";
+
+    var selection = new NGL.Selection( sele );
+
+    var selectionObj = {
+        "operator": "OR",
+        "rules": [
+            { "resname": "MET" },
+            {
+                "operator": "AND",
+                "negate": true,
+                "rules": [
+                    { "resno": [ 10, 15 ] },
+                    { "resno": [ 15, 20 ] }
+                ]
+            },
+            { "resname": "GLU" },
+        ]
+    };
+
+    assert.deepEqual( selection.selection, selectionObj, "Passed!" );
+
+});
+
+
+QUnit.test( "selection negate parens 4", function( assert ) {
+
+    var sele = "MET or not ( 10-15 and 15-20 ) and GLU";
+
+    var selection = new NGL.Selection( sele );
+
+    var selectionObj = {
+        "operator": "OR",
+        "rules": [
+            { "resname": "MET" },
+            {
+                "operator": "AND",
+                "rules": [
+                    {
+                        "operator": "AND",
+                        "negate": true,
+                        "rules": [
+                            { "resno": [ 10, 15 ] },
+                            { "resno": [ 15, 20 ] }
+                        ]
+                    },
+                    { "resname": "GLU" }
+                ]
+            }
+        ]
+    };
+
+    assert.deepEqual( selection.selection, selectionObj, "Passed!" );
+
+});
+
+
+QUnit.test( "selection negate parens 5", function( assert ) {
+
+    var sele = "1-100 and not ( MET or GLU ) or 300-330";
+
+    var selection = new NGL.Selection( sele );
+
+    var selectionObj = {
+        "operator": "OR",
+        "rules": [
+            {
+                "operator": "AND",
+                "rules": [
+                    { "resno": [ 1, 100 ] },
+                    {
+                        "operator": "OR",
+                        "negate": true,
+                        "rules": [
+                            { "resname": "MET" },
+                            { "resname": "GLU" }
+                        ]
+                    }
+                ]
+            },
+            { "resno": [ 300, 330 ] }
+        ]
     };
 
     assert.deepEqual( selection.selection, selectionObj, "Passed!" );
