99c9a1d0434c49c39c4276339da06c5d0dea5786
Alexander Rose <alexander.rose@weirdbyte.de>
2014-08-20 16:56:32 +0200

    CODE: removed unused files or moved to tmp/

--- jsmol/j2s/J/adapter/readers/cif/MSCifInterface.js
+++
@@ -1,4 +0,0 @@
-Clazz.declarePackage ("J.adapter.readers.cif");
-Clazz.load (["J.adapter.smarter.MSInterface"], "J.adapter.readers.cif.MSCifInterface", null, function () {
-Clazz.declareInterface (J.adapter.readers.cif, "MSCifInterface", J.adapter.smarter.MSInterface);
-});
