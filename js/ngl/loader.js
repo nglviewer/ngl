29087955fbd9b8667352ece6c14d245dd5531a0e
Alexander Rose <alexander.rose@weirdbyte.de>
2015-09-27 14:30:21 +0200

    CHANGE: moved mdsrv datasource to its own file

--- js/ngl/loader.js
+++ js/ngl/loader.js
@@ -68,51 +68,6 @@ NGL.StaticDatasource = function( baseUrl ){
 };
 
 
-NGL.MdsrvDatasource = function( baseUrl ){
-
-    baseUrl = baseUrl || "";
-
-    this.getListing = function( path ){
-        path = path || "";
-        var url = baseUrl + "dir/" + path;
-        return NGL.autoLoad( url, {
-            ext: "json", noWorker: true
-        } ).then( function( jsonData ){
-            return {
-                path: path,
-                data: jsonData.data
-            };
-        } );
-    };
-
-    this.getUrl = function( src ){
-        var info = NGL.getFileInfo( src );
-        return baseUrl + "file/" + info.path;
-    };
-
-    this.getNumframesUrl = function( src ){
-        var info = NGL.getFileInfo( src );
-        return baseUrl + "traj/numframes/" + info.path;
-    };
-
-    this.getFrameUrl = function( src, frameIndex ){
-        var info = NGL.getFileInfo( src );
-        return baseUrl + "traj/frame/" + frameIndex + "/" + info.path;
-    };
-
-    this.getFrameParams = function( src, atomIndices ){
-        var info = NGL.getFileInfo( src );
-        return "atomIndices=" + atomIndices.join(";");
-    };
-
-    this.getPathUrl = function( src, atomIndex ){
-        var info = NGL.getFileInfo( src );
-        return baseUrl + "traj/path/" + atomIndex + "/" + info.path;
-    };
-
-};
-
-
 NGL.RcsbDatasource = function(){
 
     var baseUrl = "http://www.rcsb.org/pdb/files/";
