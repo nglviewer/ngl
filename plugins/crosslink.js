bd85c7994155c413cb3bdeb83b856933d1b476d8
arose <alexander.rose@weirdbyte.de>
2015-10-22 21:55:38 +0200

    PLUGIN: crosslink, basic parameter handling

--- plugins/crosslink.js
+++ plugins/crosslink.js
@@ -120,7 +120,41 @@ CrosslinkData.prototype = {
 };
 
 
-var CrosslinkRepresentation = function( stage, structureComp, xlList ){
+var CrosslinkRepresentation = function( stage, structureComp, xlList, params ){
+
+    var p = Object.assign( {}, params );
+
+    if( p.displayedResiduesColor === undefined && p.displayedColor === undefined ){
+        p.displayedResiduesColor = "lightgrey";
+    }
+    if( p.displayedLinksColor === undefined && p.displayedColor === undefined ){
+        p.displayedLinksColor = "lightgrey";
+    }
+    if( p.highlightedResiduesColor === undefined && p.highlightedColor === undefined ){
+        p.highlightedResiduesColor = "lightgreen";
+    }
+    if( p.highlightedLinksColor === undefined && p.highlightedColor === undefined ){
+        p.highlightedLinksColor = "lightgreen";
+    }
+    if( p.sstrucColor === undefined ){
+        p.sstrucColor = "wheat";
+    }
+    if( p.displayedDistanceColor === undefined ){
+        p.displayedDistanceColor = "tomato";
+    }
+    if( p.highlightedDistanceColor === undefined ){
+        p.highlightedDistanceColor = "white";
+    }
+    if( p.displayedDistanceVisible === undefined ){
+        p.displayedDistanceVisible = false;
+    }
+    if( p.highlightedDistanceVisible === undefined ){
+        p.highlightedDistanceVisible = true;
+    }
+
+    this.setParameters( p, true );
+
+    //
 
     this.signals = {
         onPicking: new signals.Signal()
@@ -213,20 +247,20 @@ CrosslinkRepresentation.prototype = {
         );
 
         this.sstrucRepr = comp.addRepresentation( "cartoon", {
-            color: "residueindex",
+            color: this.sstrucColor,
             name: "sstruc"
         } );
 
         this.resRepr = comp.addRepresentation( "spacefill", {
             sele: resSele,
-            color: "lightgrey",  // this.linkCountScheme,
+            color: this.displayedResiduesColor,
             scale: 0.6,
             name: "res"
         } );
 
         this.resEmphRepr = comp.addRepresentation( "spacefill", {
             sele: "none",
-            color: "fuchsia",
+            color: this.highlightedResiduesColor,
             scale: 0.9,
             opacity: 0.7,
             name: "resEmph"
@@ -244,15 +278,19 @@ CrosslinkRepresentation.prototype = {
 
         this.linkRepr = comp.addRepresentation( "distance", {
             atomPair: xlPair,
-            color: "lightgrey",
-            labelSize: 0.001,
+            color: this.displayedLinksColor,
+            labelSize: 2.0,
+            labelColor: this.displayedDistanceColor,
+            labelVisible: this.displayedDistanceVisible,
             name: "link"
         } );
 
         this.linkEmphRepr = comp.addRepresentation( "distance", {
             atomPair: [],
-            color: "fuchsia",
+            color: this.highlightedLinksColor,
             labelSize: 2.0,
+            labelColor: this.highlightedDistanceColor,
+            labelVisible: this.highlightedDistanceVisible,
             scale: 2.5,
             opacity: 0.6,
             name: "linkEmph"
@@ -367,11 +405,107 @@ CrosslinkRepresentation.prototype = {
 
     },
 
-    setParameters: function( params ){
+    /**
+     * params
+     *
+     * - displayedColor (sets residues and links color)
+     * - highlightedColor (sets residues and links color)
+     * - displayedResiduesColor
+     * - highlightedResiduesColor
+     * - displayedLinksColor
+     * - highlightedLinksColor
+     * - sstrucColor
+     * - displayedDistanceColor (can't be a color scheme)
+     * - highlightedDistanceColor (can't be a color scheme)
+     * - displayedDistanceVisible
+     * - highlightedDistanceVisible
+     */
+    setParameters: function( params, initialize ){
 
-        // highlightColor
-        // linkColor
-        // distance label display
+        var p = Object.assign( {}, params );
+
+        var resParams = {};
+        var linkParams = {};
+        var resEmphParams = {};
+        var linkEmphParams = {};
+        var sstrucParams = {};
+
+        // set params
+
+        resParams.color = p.displayedColor;
+        linkParams.color = p.displayedColor;
+        resEmphParams.color = p.highlightedColor;
+        linkEmphParams.color = p.highlightedColor;
+
+        if( p.displayedResiduesColor ){
+            resParams.color = p.displayedResiduesColor;
+        }
+        if( p.displayedLinksColor ){
+            linkParams.color = p.displayedLinksColor;
+        }
+        if( p.highlightedResiduesColor ){
+            resEmphParams.color = p.highlightedResiduesColor;
+        }
+        if( p.highlightedLinksColor ){
+            linkEmphParams.color = p.highlightedLinksColor;
+        }
+
+        sstrucParams.color = p.sstrucColor;
+
+        linkParams.labelColor = p.displayedDistanceColor;
+        linkEmphParams.labelColor = p.highlightedDistanceColor;
+        linkParams.labelVisible = p.displayedDistanceVisible;
+        linkEmphParams.labelVisible = p.highlightedDistanceVisible;
+
+        // set object properties
+
+        if( resParams.color !== undefined ){
+            this.displayedResiduesColor = resParams.color;
+        }
+        if( linkParams.color !== undefined ){
+            this.displayedLinksColor = linkParams.color;
+        }
+        if( resEmphParams.color !== undefined ){
+            this.highlightedResiduesColor = resEmphParams.color;
+        }
+        if( linkEmphParams.color !== undefined ){
+            this.highlightedLinksColor = linkEmphParams.color;
+        }
+
+        if( sstrucParams.color !== undefined ){
+            this.sstrucColor = sstrucParams.color;
+        }
+
+        if( linkParams.labelColor !== undefined ){
+            this.displayedDistanceColor = linkParams.labelColor;
+        }
+        if( linkEmphParams.labelColor !== undefined ){
+            this.highlightedDistanceColor = linkEmphParams.labelColor;
+        }
+        if( linkParams.labelVisible !== undefined ){
+            this.displayedDistanceVisible = linkParams.labelVisible;
+        }
+        if( linkEmphParams.labelVisible !== undefined ){
+            this.highlightedDistanceVisible = linkEmphParams.labelVisible;
+        }
+
+        // pass params to representations
+
+        if( !initialize ){
+
+            this.resRepr.setColor( resParams.color );
+            this.linkRepr.setColor( linkParams.color );
+            this.resEmphRepr.setColor( resEmphParams.color );
+            this.linkEmphRepr.setColor( linkEmphParams.color );
+            this.sstrucRepr.setColor( sstrucParams.color );
+
+            this.resRepr.setParameters( resParams );
+            this.linkRepr.setParameters( linkParams );
+            this.resEmphRepr.setParameters( resEmphParams );
+            this.linkEmphRepr.setParameters( linkEmphParams );
+            this.sstrucRepr.setParameters( sstrucParams );
+
+        }
 
     },
 
