6892f887c7b965d0a400205c15e1f3a2e4b26de5
Alexander Rose <alex.rose@rcsb.org>
2016-02-02 18:24:14 -0800

    tweaked buildUnitcellAssembly function

--- js/ngl/parser.js
+++ js/ngl/parser.js
@@ -741,7 +741,7 @@ NGL.buildUnitcellAssembly = function( structure ){
     var uc = structure.unitcell;
     var biomolDict = structure.biomolDict;
 
-    var centerFrac = structure.atomCenter().applyMatrix4( uc.cartToFrac );
+    var centerFrac = structure.center.clone().applyMatrix4( uc.cartToFrac );
     var symopDict = NGL.getSymmetryOperations( uc.spacegroup );
 
     var positionFrac = new THREE.Vector3();
@@ -794,39 +794,41 @@ NGL.buildUnitcellAssembly = function( structure ){
         chainList: undefined
     };
 
+    var vec = new THREE.Vector3();
+
     biomolDict[ "SUPERCELL" ] = {
         matrixDict: Object.assign( {},
             getOpDict(),
-            getOpDict( new THREE.Vector3(  1,  1,  1 ), "_666" ),
-            getOpDict( new THREE.Vector3( -1, -1, -1 ), "_444" ),
+            getOpDict( vec.set(  1,  1,  1 ), "_666" ),
+            getOpDict( vec.set( -1, -1, -1 ), "_444" ),
 
-            getOpDict( new THREE.Vector3(  1,  0,  0 ), "_655" ),
-            getOpDict( new THREE.Vector3(  1,  1,  0 ), "_665" ),
-            getOpDict( new THREE.Vector3(  1,  0,  1 ), "_656" ),
-            getOpDict( new THREE.Vector3(  0,  1,  0 ), "_565" ),
-            getOpDict( new THREE.Vector3(  0,  1,  1 ), "_566" ),
-            getOpDict( new THREE.Vector3(  0,  0,  1 ), "_556" ),
+            getOpDict( vec.set(  1,  0,  0 ), "_655" ),
+            getOpDict( vec.set(  1,  1,  0 ), "_665" ),
+            getOpDict( vec.set(  1,  0,  1 ), "_656" ),
+            getOpDict( vec.set(  0,  1,  0 ), "_565" ),
+            getOpDict( vec.set(  0,  1,  1 ), "_566" ),
+            getOpDict( vec.set(  0,  0,  1 ), "_556" ),
 
-            getOpDict( new THREE.Vector3( -1,  0,  0 ), "_455" ),
-            getOpDict( new THREE.Vector3( -1, -1,  0 ), "_445" ),
-            getOpDict( new THREE.Vector3( -1,  0, -1 ), "_454" ),
-            getOpDict( new THREE.Vector3(  0, -1,  0 ), "_545" ),
-            getOpDict( new THREE.Vector3(  0, -1, -1 ), "_544" ),
-            getOpDict( new THREE.Vector3(  0,  0, -1 ), "_554" ),
+            getOpDict( vec.set( -1,  0,  0 ), "_455" ),
+            getOpDict( vec.set( -1, -1,  0 ), "_445" ),
+            getOpDict( vec.set( -1,  0, -1 ), "_454" ),
+            getOpDict( vec.set(  0, -1,  0 ), "_545" ),
+            getOpDict( vec.set(  0, -1, -1 ), "_544" ),
+            getOpDict( vec.set(  0,  0, -1 ), "_554" ),
 
-            getOpDict( new THREE.Vector3(  1, -1, -1 ), "_644" ),
-            getOpDict( new THREE.Vector3(  1,  1, -1 ), "_664" ),
-            getOpDict( new THREE.Vector3(  1, -1,  1 ), "_646" ),
-            getOpDict( new THREE.Vector3( -1,  1,  1 ), "_466" ),
-            getOpDict( new THREE.Vector3( -1, -1,  1 ), "_446" ),
-            getOpDict( new THREE.Vector3( -1,  1, -1 ), "_464" ),
+            getOpDict( vec.set(  1, -1, -1 ), "_644" ),
+            getOpDict( vec.set(  1,  1, -1 ), "_664" ),
+            getOpDict( vec.set(  1, -1,  1 ), "_646" ),
+            getOpDict( vec.set( -1,  1,  1 ), "_466" ),
+            getOpDict( vec.set( -1, -1,  1 ), "_446" ),
+            getOpDict( vec.set( -1,  1, -1 ), "_464" ),
 
-            getOpDict( new THREE.Vector3(  0,  1, -1 ), "_564" ),
-            getOpDict( new THREE.Vector3(  0, -1,  1 ), "_546" ),
-            getOpDict( new THREE.Vector3(  1,  0, -1 ), "_654" ),
-            getOpDict( new THREE.Vector3( -1,  0,  1 ), "_456" ),
-            getOpDict( new THREE.Vector3(  1, -1,  0 ), "_645" ),
-            getOpDict( new THREE.Vector3( -1,  1,  0 ), "_465" )
+            getOpDict( vec.set(  0,  1, -1 ), "_564" ),
+            getOpDict( vec.set(  0, -1,  1 ), "_546" ),
+            getOpDict( vec.set(  1,  0, -1 ), "_654" ),
+            getOpDict( vec.set( -1,  0,  1 ), "_456" ),
+            getOpDict( vec.set(  1, -1,  0 ), "_645" ),
+            getOpDict( vec.set( -1,  1,  0 ), "_465" )
         ),
         chainList: undefined
     };
