c8bfed2716affe71d8e8fc491a1bb5b2074a3873
Alexander Rose <alex.rose@rcsb.org>
2016-03-25 17:03:08 -0700

    optimized nearClip test in shaders

--- shader/chunk/nearclip_vertex.glsl
+++ shader/chunk/nearclip_vertex.glsl
@@ -1,5 +1,4 @@
 #ifdef NEAR_CLIP
-    // move out of viewing frustum for custom clipping
-    if( dot( vec4( vViewPosition, 1.0 ), vec4( 0.0, 0.0, -1.0, nearClip ) ) > 0.0 )
-        gl_Position.w = -10.0;
+    if( vViewPosition.z < nearClip )
+        gl_Position.z = 2.0 * gl_Position.w;  // move out of [ -w, +w ]
 #endif
