20465c88080ba139868416d429ce82be6c5c1909
Alexander Rose <alex.rose@rcsb.org>
2016-03-31 19:02:11 -0700

    updated mmtf backbone/reduced url

--- js/ngl/loader.js
+++ js/ngl/loader.js
@@ -77,7 +77,7 @@ NGL.RcsbDatasource = function(){
     var baseUrl = "http://files.rcsb.org/download/";
     // var baseUrl = "http://www.rcsb.org/pdb/files/";
     var mmtfBaseUrl = "http://mmtf.rcsb.org/full/";
-    var bbMmtfBaseUrl = "http://mmtf.rcsb.org/backbone/";
+    var bbMmtfBaseUrl = "http://mmtf.rcsb.org/reduced/";
 
     this.getUrl = function( src ){
         // valid path are
