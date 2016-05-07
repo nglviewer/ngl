d484aa48d70a0023d4f83ac77567528b42d586ae
arose <alexander.rose@weirdbyte.de>
2016-05-07 16:10:13 -0700

    removed console.log statement

--- js/ngl/viewer.js
+++ js/ngl/viewer.js
@@ -2164,8 +2164,6 @@ NGL.Viewer.prototype = {
 
     setOrientation: function( orientation ){
 
-        console.log( "setOrientation" );
-
         // remove any paning/translation
         this.controls.object.position.sub( this.controls.target );
         this.controls.target.copy( this.controls.target0 );
