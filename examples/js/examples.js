ac88f580e597c03bcbe87670469d81644a0567b9
Alexander Rose <alex.rose@rcsb.org>
2016-06-08 10:39:24 -0700

    tweaked hiv example

--- examples/js/examples.js
+++ examples/js/examples.js
@@ -635,12 +635,27 @@ NGL.ExampleRegistry.addDict( {
 
     "hiv": function( stage ){
 
-        stage.loadFile( "rcsb://3j3y.mmtf" ).then( function( o ){
+        stage.loadFile( "rcsb://3j3q.mmtf" ).then( function( o ){
 
             o.addRepresentation( "surface", {
                 surfaceType: "sas",
                 smooth: 2,
                 scaleFactor: 0.2,
+                colorScheme: "chainindex"
+            } );
+
+            o.addRepresentation( "cartoon", {
+                sele: ":f0 or :f1 or :f2 or :f3 or :f4 or :f5",
+                colorScheme: "chainindex"
+            } );
+
+            o.addRepresentation( "ball+stick", {
+                sele: ":f0",
+                colorScheme: "element"
+            } );
+
+            o.addRepresentation( "rocket", {
+                sele: ":f0",
                 colorScheme: "chainindex"
             } );
 
