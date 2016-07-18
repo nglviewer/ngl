3a4b0f41670a9b822a140a2f6f96ea3e74240593
Alexander Rose <alex.rose@rcsb.org>
2016-07-18 11:22:28 -0700

    tweaked bfactor example

--- examples/js/examples.js
+++ examples/js/examples.js
@@ -396,12 +396,13 @@ NGL.ExampleRegistry.addDict( {
         stage.loadFile( "data://1u19.pdb" ).then( function( o ){
 
             o.addRepresentation( "tube", {
-                sele: ":A", radius: "bfactor", scale: 0.010, color: "bfactor"
+                sele: ":A", radius: "bfactor", scale: 0.010,
+                color: "bfactor", colorScale: "RdYlBu"
             } );
 
             o.addRepresentation( "ball+stick", {
                 sele: ":A and sidechainAttached", aspectRatio: 1.5,
-                color: "bfactor"
+                color: "bfactor", colorScale: "RdYlBu"
             } );
 
             o.centerView( true, ":A" );
