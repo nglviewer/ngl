67dea35e14d44e2755990584fad2896059a49b2b
Alexander Rose <alex.rose@rcsb.org>
2016-10-03 14:06:46 -0700

    fixed nearclip vertex too greedy

--- src/shader/chunk/nearclip_vertex.glsl
+++ src/shader/chunk/nearclip_vertex.glsl
@@ -1,5 +1,5 @@
 #ifdef NEAR_CLIP
-    if( vViewPosition.z < nearClip )
+    if( vViewPosition.z < nearClip - 5.0 )
     	// move out of [ -w, +w ]
         gl_Position.z = 2.0 * gl_Position.w;
 #endif
