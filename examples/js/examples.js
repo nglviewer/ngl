c8ff685be26c17ee475c1fa84b3c48f7ee7718f2
Alexander Rose <alex.rose@rcsb.org>
2017-03-16 11:25:48 -0700

    fixed ‘spatialHash’ example

--- examples/js/examples.js
+++ examples/js/examples.js
@@ -1408,35 +1408,31 @@ NGL.ExampleRegistry.addDict( {
 
             function getCenterArray(){
                 var position = new NGL.Vector3();
-                var target = stage.viewer.controls.target;
-                var group = stage.viewer.rotationGroup.position;
-                position.copy( group ).negate().add( target );
-                return position;
+                position.copy( stage.viewerControls.position );
+                return position.negate();
             }
 
-            var sphereBuffer = new NGL.SphereBuffer(
-                new Float32Array( getCenterArray().toArray() ),
-                new Float32Array( [ 1, 0.5, 1 ] ),
-                new Float32Array( [ radius ] )
-            );
+            var sphereBuffer = new NGL.SphereBuffer( {
+                position: new Float32Array( getCenterArray().toArray() ),
+                color: new Float32Array( [ 1, 0.5, 1 ] ),
+                radius: new Float32Array( [ radius ] )
+            } );
             o.addBufferRepresentation( sphereBuffer, { opacity: 0.5 } );
 
             var prevSele = "";
             var prevPos = new NGL.Vector3( Infinity, Infinity, Infinity );
-            stage.viewer.controls.addEventListener(
-                'change', function(){
-                    var pos = getCenterArray();
-                    if( pos.distanceTo( prevPos ) > 0.1 ){
-                        sphereBuffer.setAttributes( { "position": pos.toArray() } );
-                        prevPos = pos;
-                        var sele = o.structure.getAtomSetWithinPoint( pos, radius ).toSeleString();
-                        if( sele !== prevSele ){
-                            spacefillRepr.setSelection( sele );
-                            prevSele = sele;
-                        }
+            stage.viewerControls.signals.changed.add( function(){
+                var pos = getCenterArray();
+                if( pos.distanceTo( prevPos ) > 0.1 ){
+                    sphereBuffer.setAttributes( { "position": pos.toArray() } );
+                    prevPos = pos;
+                    var sele = o.structure.getAtomSetWithinPoint( pos, radius ).toSeleString();
+                    if( sele !== prevSele ){
+                        spacefillRepr.setSelection( sele );
+                        prevSele = sele;
                     }
                 }
-            );
+            } );
 
         } );
 
