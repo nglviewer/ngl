10a4b89778fb4f515da093fbf735d3e6e1d2b709
Alexander Rose <alex.rose@rcsb.org>
2017-04-19 11:55:53 -0700

    added Math.sign polyfill

--- src/polyfills.js
+++ src/polyfills.js
@@ -85,6 +85,23 @@ Math.cbrt = Math.cbrt || function( x ){
 };
 
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sign
+if( !Math.sign ){
+    Math.sign = function( x ){
+        // If x is NaN, the result is NaN.
+        // If x is -0, the result is -0.
+        // If x is +0, the result is +0.
+        // If x is negative and not -0, the result is -1.
+        // If x is positive and not +0, the result is +1.
+        x = +x; // convert to a number
+        if( x === 0 || isNaN( x ) ){
+          return Number( x );
+        }
+        return x > 0 ? 1 : -1;
+    };
+}
+
+
 if( !Number.isInteger ){
 
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
