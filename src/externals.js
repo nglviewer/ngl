54d0934376cbc3a6447ab2e4c945a27e7d90c6ce
Alexander Rose <alex.rose@rcsb.org>
2017-04-26 17:57:12 -0700

    docs for globals, externals, version

---
+++ src/externals.js
@@ -0,0 +1,42 @@
+/**
+ * @file Externals
+ * @private
+ * @author Alexander Rose <alexander.rose@weirdbyte.de>
+ */
+
+
+/**
+ * @external {Signal} https://millermedeiros.github.io/js-signals/docs/symbols/Signal.html
+ */
+
+/**
+ * @external {Matrix3} https://threejs.org/docs/#api/math/Matrix3
+ */
+
+/**
+ * @external {Matrix4} https://threejs.org/docs/#api/math/Matrix4
+ */
+
+/**
+ * @external {Vector2} https://threejs.org/docs/#api/math/Vector2
+ */
+
+/**
+ * @external {Vector3} https://threejs.org/docs/#api/math/Vector3
+ */
+
+/**
+ * @external {Box3} https://threejs.org/docs/#api/math/Box3
+ */
+
+/**
+ * @external {Quaternion} https://threejs.org/docs/#api/math/Quaternion
+ */
+
+/**
+ * @external {Plane} https://threejs.org/docs/#api/math/Plane
+ */
+
+/**
+ * @external {Color} https://threejs.org/docs/#api/math/Color
+ */
---
+++ src/version.js
@@ -0,0 +1,17 @@
+/**
+ * @file Version
+ * @private
+ * @author Alexander Rose <alexander.rose@weirdbyte.de>
+ */
+
+import { version } from "../package.json";
+
+
+/**
+ * Version name
+ * @type {String}
+ */
+const Version = version;
+
+
+export default Version;
