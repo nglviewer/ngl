9d235accfb4d4c8cc422306b77909a1f3f2be1be
Alexander Rose <alex.rose@rcsb.org>
2017-04-27 17:09:11 -0700

    cleanup

--- examples/js/examples.js
+++ examples/js/examples.js
@@ -1662,18 +1662,18 @@ NGL.ExampleRegistry.addDict( {
 
     "validation": function( stage ){
 
-        Promise.all([
+        Promise.all( [
             stage.loadFile( "data://3PQR.cif" ),
             NGL.autoLoad( "data://3pqr_validation.xml", { ext: "validation" } )
-        ]).then( function( ol ){
-            ol[0].structure.validation = ol[1];
-            ol[0].addRepresentation( "cartoon", { color: "geoquality" } );
-            ol[0].addRepresentation( "validation" );
-            ol[0].addRepresentation( "ball+stick", {
-                sele: ol[1].clashSele,
+        ] ).then( function( ol ){
+            ol[ 0 ].structure.validation = ol[ 1 ];
+            ol[ 0 ].addRepresentation( "cartoon", { color: "geoquality" } );
+            ol[ 0 ].addRepresentation( "validation" );
+            ol[ 0 ].addRepresentation( "ball+stick", {
+                sele: ol[ 1 ].clashSele,
                 color: "geoquality"
             } );
-            ol[0].addRepresentation( "licorice", {
+            ol[ 0 ].addRepresentation( "licorice", {
                 sele: "hetero",
                 color: "geoquality"
             } );
