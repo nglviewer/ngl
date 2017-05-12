b0cbf6a7460717cae03464cc88841b0662847f90
Alexander Rose <alex.rose@rcsb.org>
2017-05-12 11:13:26 -0700

    tweaked codepen css

--- scripts/js/slimer/test.js
+++ scripts/js/slimer/test.js
@@ -72,8 +72,7 @@ const html = (
 );
 
 const css = (
-    "* { margin: 0; padding: 0; }\n" +
-    "html, body { width: 100%; height: 100%; overflow: hidden; }\n"
+    "html, body { width: 100%; height: 100%; overflow: hidden; margin: 0; padding: 0; }\n"
 );
 
 const jsPrefix = (
