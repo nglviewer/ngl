97b5e5488601204a2f501b908e3fb1090149ece3
Alexander Rose <alex.rose@rcsb.org>
2017-05-24 21:55:27 -0700

    added xray test script

---
+++ examples/scripts/test/xray.js
@@ -0,0 +1,36 @@
+
+stage.loadFile( "data://3ek3.cif" ).then( function( o ){
+    o.addRepresentation( "licorice", {
+        colorValue: "yellow",
+        roughness: 1.0
+    } );
+    o.autoView( "FMN" );
+    stage.setFocus( 97 );
+} );
+
+stage.loadFile( "data://3ek3-2fofc.map.gz" ).then( function( o ){
+    o.addRepresentation( "surface", {
+        color: "skyblue",
+        isolevel: 2.5,
+        boxSize: 10,
+        wireframe: true,
+        contour: true
+    } );
+} );
+
+stage.loadFile( "data://3ek3-fofc.map.gz" ).then( function( o ){
+    o.addRepresentation( "surface", {
+        color: "lightgreen",
+        isolevel: 2,
+        boxSize: 10,
+        wireframe: true,
+        contour: true
+    }  );
+    o.addRepresentation( "surface", {
+        color: "tomato",
+        isolevel: -2,
+        boxSize: 10,
+        wireframe: true,
+        contour: true
+    }  );
+} );
