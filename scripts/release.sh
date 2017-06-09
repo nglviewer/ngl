61483c0c2f87d082e3a34c04154a4809376ba218
Alexander Rose <alex.rose@rcsb.org>
2017-06-08 22:51:28 -0700

    fixed release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 LEVEL=$1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+
+cd ${DIR};
 
 npm version $LEVEL;
 npm run-script doc;
 npm run-script gallery;
 npm run-script deploy $LEVEL;
+./deploy.sh $LEVEL;
 if [ "$LEVEL" = "prerelease" ]; then
 	npm publish --tag next;
 else
