fd0a3f3a4f3b366c8d058735de72b345f7f79f91
arose <alexander.rose@weirdbyte.de>
2017-07-02 20:41:57 -0700

    added PORT arg to gallery shell script

--- scripts/gallery.sh
+++ scripts/gallery.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+PORT=$1
+if [ "$PORT" = "" ]; then
+	PORT="8091"
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
 cd ${DIR};
 
-slimerjs js/slimer/gallery.js port:8091
+slimerjs js/slimer/gallery.js port:${PORT}
