ff7a1c73a3740b843faf639bf64217ff59d07e03
arose <alexander.rose@weirdbyte.de>
2017-07-04 11:40:55 -0700

    added String.prototype.repeat polyfill

--- src/polyfills.js
+++ src/polyfills.js
@@ -215,6 +215,51 @@ if (!String.prototype.endsWith) {
   }
 }
 
+if (!String.prototype.repeat) {
+  // eslint-disable-next-line no-extend-native
+  String.prototype.repeat = function (count) {
+    'use strict'
+    if (this === null) {
+      throw new TypeError('can\'t convert ' + this + ' to object')
+    }
+    var str = '' + this
+    count = +count
+    if (Number.isNaN(count)) {
+      count = 0
+    }
+    if (count < 0) {
+      throw new RangeError('repeat count must be non-negative')
+    }
+    if (count === Infinity) {
+      throw new RangeError('repeat count must be less than infinity')
+    }
+    count = Math.floor(count)
+    if (str.length === 0 || count === 0) {
+      return ''
+    }
+    // Ensuring count is a 31-bit integer allows us to heavily optimize the
+    // main part. But anyway, most current (August 2014) browsers can't handle
+    // strings 1 << 28 chars or longer, so:
+    if (str.length * count >= 1 << 28) {
+      throw new RangeError('repeat count must not overflow maximum string size')
+    }
+    var rpt = ''
+    for (;;) {
+      if ((count & 1) === 1) {
+        rpt += str
+      }
+      count >>>= 1
+      if (count === 0) {
+        break
+      }
+      str += str
+    }
+    // Could we try:
+    // return Array(count + 1).join(this);
+    return rpt
+  }
+}
+
 if (!String.prototype.includes) {
   // eslint-disable-next-line no-extend-native
   String.prototype.includes = function (search, start) {
