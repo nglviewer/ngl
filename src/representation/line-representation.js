ea58133f0a47d3b9236cebc704b6e3baa9ef4788
Fred Ludlow <fred.ludlow@astx.com>
2017-09-05 16:42:28 +0100

    Added crosses option to line representation

--- src/representation/line-representation.js
+++ src/representation/line-representation.js
@@ -8,6 +8,19 @@ import { defaults } from '../utils.js'
 import { RepresentationRegistry } from '../globals.js'
 import StructureRepresentation from './structure-representation.js'
 import WideLineBuffer from '../buffer/wideline-buffer.js'
+import { AtomPicker } from '../utils/picker.js'
+
+function getIsolatedAtomSet (structure) {
+  const atomSet = structure.getAtomSet()
+  const bondSet = structure.getBondSet()
+  const bp = structure.getBondProxy()
+  bondSet.forEach(function (idx) {
+    bp.index = idx
+    atomSet.clear(bp.atomIndex1)
+    atomSet.clear(bp.atomIndex2)
+  })
+  return atomSet
+}
 
 /**
  * Line representation
@@ -49,6 +62,21 @@ class LineRepresentation extends StructureRepresentation {
       },
       linewidth: {
         type: 'integer', max: 50, min: 1, buffer: true
+      },
+      lines: {
+        type: 'boolean', rebuild: true
+      },
+      crosses: {
+        type: 'select',
+        rebuild: true,
+        options: {
+          'off': 'off',
+          'isolated': 'isolated',
+          'all': 'all'
+        }
+      },
+      crossSize: {
+        type: 'number', precision: 2, max: 1.0, min: 0.05, rebuild: true
       }
 
     }, this.parameters, {
@@ -71,6 +99,9 @@ class LineRepresentation extends StructureRepresentation {
     this.multipleBond = defaults(p.multipleBond, 'off')
     this.bondSpacing = defaults(p.bondSpacing, 1.0)
     this.linewidth = defaults(p.linewidth, 2)
+    this.lines = defaults(p.lines, true)
+    this.crosses = defaults(p.crosses, 'isolated')
+    this.crossSize = defaults(p.crossSize, 0.2)
 
     super.init(p)
   }
@@ -85,34 +116,165 @@ class LineRepresentation extends StructureRepresentation {
     return super.getBondParams(what, params)
   }
 
+  _crossData (what, sview) {
+    if (what) {
+      if (!what.position && !what.color) return
+    }
+
+    const p = {}
+    if (this.crosses === 'isolated') {
+      p.atomSet = getIsolatedAtomSet(sview)
+    }
+
+    const atomData = sview.getAtomData(this.getAtomParams(what, p))
+    const crossData = {}
+    const position = atomData.position
+    const color = atomData.color
+    const picking = atomData.picking
+
+    const size = (position || color).length
+    const attrSize = size * 3
+
+    let cPosition1
+    let cPosition2
+    let cColor
+    let cColor2
+    let cSize
+
+    let pickingArray
+
+    if (!what || what.position) {
+      cPosition1 = crossData.position1 = new Float32Array(attrSize)
+      cPosition2 = crossData.position2 = new Float32Array(attrSize)
+      cSize = this.crossSize
+    }
+    if (!what || what.color) {
+      cColor = crossData.color = new Float32Array(attrSize)
+      cColor2 = crossData.color2 = new Float32Array(attrSize)
+    }
+    if (!what || what.picking) {
+      pickingArray = new Float32Array(atomData.picking.array.length * 3) // Needs padding??
+    }
+
+    for (let v = 0; v < size; v++) {
+      const j = v * 3
+      const i = j * 3
+
+      if (!what || what.position) {
+        const x = position[ j ]
+        const y = position[ j + 1 ]
+        const z = position[ j + 2 ]
+
+        cPosition1[ i ] = x - cSize
+        cPosition1[ i + 1 ] = y
+        cPosition1[ i + 2 ] = z
+        cPosition2[ i ] = x + cSize
+        cPosition2[ i + 1 ] = y
+        cPosition2[ i + 2 ] = z
+
+        cPosition1[ i + 3 ] = x
+        cPosition1[ i + 4 ] = y - cSize
+        cPosition1[ i + 5 ] = z
+        cPosition2[ i + 3 ] = x
+        cPosition2[ i + 4 ] = y + cSize
+        cPosition2[ i + 5 ] = z
+
+        cPosition1[ i + 6 ] = x
+        cPosition1[ i + 7 ] = y
+        cPosition1[ i + 8 ] = z - cSize
+        cPosition2[ i + 6 ] = x
+        cPosition2[ i + 7 ] = y
+        cPosition2[ i + 8 ] = z + cSize
+      }
+
+      if (!what || what.color) {
+        const cimax = i + 9
+        for (let ci = i; ci < cimax; ci += 3) {
+          cColor[ ci ] = cColor2[ ci ] = color[ j ]
+          cColor[ ci + 1 ] = cColor2[ ci + 1 ] = color[ j + 1 ]
+          cColor[ ci + 2 ] = cColor2[ ci + 2 ] = color[ j + 2 ]
+        }
+      }
+
+      if (!what || what.picking) {
+        pickingArray[ j ] =
+        pickingArray[ j + 1 ] =
+        pickingArray[ j + 2 ] = picking.array[ v ]
+      }
+    }
+
+    if (!what || what.picking) {
+      crossData.picking = new AtomPicker(
+        pickingArray, atomData.picking.structure
+      )
+    }
+
+    return crossData
+  }
+
   createData (sview) {
     var what = { position: true, color: true, picking: true }
-    var bondData = sview.getBondData(this.getBondParams(what))
 
-    var lineBuffer = new WideLineBuffer(
-      bondData, this.getBufferParams({ linewidth: this.linewidth })
-    )
+    var bufferList = []
+
+    if (this.lines) {
+      var bondData = sview.getBondData(this.getBondParams(what))
+
+      var lineBuffer = new WideLineBuffer(
+        bondData, this.getBufferParams({ linewidth: this.linewidth })
+      )
+
+      bufferList.push(lineBuffer)
+    }
+
+    if (this.crosses !== 'off') {
+      var crossBuffer = new WideLineBuffer(
+        this._crossData(what, sview),
+        this.getBufferParams({linewidth: this.linewidth})
+      )
+      bufferList.push(crossBuffer)
+    }
 
     return {
-      bufferList: [ lineBuffer ]
+      bufferList: bufferList
     }
   }
 
   updateData (what, data) {
-    var bondData = data.sview.getBondData(this.getBondParams(what))
-    var lineData = {}
+    var bufferIdx = 0
 
-    if (!what || what.position) {
-      lineData.position1 = bondData.position1
-      lineData.position2 = bondData.position2
+    if (this.lines) {
+      var bondData = data.sview.getBondData(this.getBondParams(what))
+      var lineData = {}
+
+      if (!what || what.position) {
+        lineData.position1 = bondData.position1
+        lineData.position2 = bondData.position2
+      }
+
+      if (!what || what.color) {
+        lineData.color = bondData.color
+        lineData.color2 = bondData.color2
+      }
+
+      data.bufferList[ bufferIdx++ ].setAttributes(lineData)
     }
 
-    if (!what || what.color) {
-      lineData.color = bondData.color
-      lineData.color2 = bondData.color2
-    }
+    if (this.crosses !== 'off') {
+      var crossAtomData = this._crossData(what, data.sview)
+      var crossData = {}
 
-    data.bufferList[ 0 ].setAttributes(lineData)
+      if (!what || what.position) {
+        crossData.position1 = crossAtomData.position1
+        crossData.position2 = crossAtomData.position2
+      }
+      if (!what || what.color) {
+        crossData.color = crossAtomData.color
+        crossData.color2 = crossAtomData.color2
+      }
+
+      data.bufferList[ bufferIdx++ ].setAttributes(crossData)
+    }
   }
 
   setParameters (params) {
