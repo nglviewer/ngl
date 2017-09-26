cda3374e0dcece1f668a4a46940331157e520799
Fred Ludlow <fred.ludlow@astx.com>
2017-09-26 17:33:47 +0100

    fix angle example

--- examples/scripts/representation/angle.js
+++ examples/scripts/representation/angle.js
@@ -1,16 +1,15 @@
-
 stage.loadFile('data://1blu.pdb').then(function (o) {
   var atomTriple = [
-        [ '1.C', '2.CA', '2.CB' ],
-        [ '2.CA', '2.C', '2.O']
+    [ '1.C', '2.CA', '2.CB' ],
+    [ '2.CA', '2.C', '2.O' ]
   ]
 
   o.addRepresentation('ball+stick', {sele: '1-2'})
   o.addRepresentation('angle', {
     atomTriple: atomTriple,
-    sdf: false,
-    color: 'skyblue'
+    labelSize: 1.0,
+    sdf: false
   })
 
-  o.autoView()
+  stage.autoView()
 })
