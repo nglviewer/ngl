5c72b6a75103ebba49eced80506772d3c6e992ab
Alexander Rose <alex.rose@rcsb.org>
2017-11-28 18:11:32 -0800

    tweaked scripts/packages

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,13 +5,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
 cd ${DIR};
 
-npm version $LEVEL;
+if [ "$LEVEL" = "ts2" ]; then
+	npm version "prerelease";
+else
+	npm version $LEVEL;
+fi
 npm run-script doc;
 npm run-script gallery;
 ./deploy.sh $LEVEL;
 cd ${DIR}/..;
 if [ "$LEVEL" = "prerelease" ]; then
 	npm publish --tag next;
+elif [ "$LEVEL" = "ts2" ]; then
+	npm publish --tag ts2;
 else
 	npm publish;
 fi
