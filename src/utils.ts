3dfc91ee70827ac788b6ca03dd5f1fd6c389257c
Alexander Rose <alex.rose@rcsb.org>
2018-01-24 16:57:12 -0800

    added deepEqual function to utils

--- src/utils.ts
+++ src/utils.ts
@@ -121,6 +121,51 @@ export function deepCopy (src: any) {
   return dst
 }
 
+export function deepEqual(a: any, b: any) {
+  // from https://github.com/epoberezkin/fast-deep-equal MIT
+  if (a === b) return true;
+
+  const arrA = Array.isArray(a)
+  const arrB = Array.isArray(b)
+
+  if (arrA && arrB) {
+    if (a.length !== b.length) return false
+    for (let i = 0; i < a.length; i++) {
+      if (!deepEqual(a[i], b[i])) return false
+    }
+    return true
+  }
+
+  if (arrA !== arrB) return false
+
+  if (a && b && typeof a === 'object' && typeof b === 'object') {
+    const keys = Object.keys(a)
+    if (keys.length !== Object.keys(b).length) return false;
+
+    const dateA = a instanceof Date
+    const dateB = b instanceof Date
+    if (dateA && dateB) return a.getTime() === b.getTime()
+    if (dateA !== dateB) return false
+
+    const regexpA = a instanceof RegExp
+    const regexpB = b instanceof RegExp
+    if (regexpA && regexpB) return a.toString() === b.toString()
+    if (regexpA !== regexpB) return false
+
+    for (let i = 0; i < keys.length; i++) {
+      if (!Object.prototype.hasOwnProperty.call(b, keys[i])) return false
+    }
+
+    for (let i = 0; i < keys.length; i++) {
+      if(!deepEqual(a[keys[i]], b[keys[i]])) return false
+    }
+
+    return true
+  }
+
+  return false
+}
+
 function openUrl (url: string) {
   const opened = window.open(url, '_blank')
   if (!opened) {
