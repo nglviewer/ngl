2b4c6309d1673484b4e1c24b01d8c823e7990535
Vincent Chen <vbchen@gmail.com>
2018-03-20 14:31:18 -0400

    updated kin.js example

--- examples/scripts/parser/kin.js
+++ examples/scripts/parser/kin.js
@@ -1,42 +1,62 @@
 
 NGL.autoLoad('data://1crnFH-multi.kin').then(function (kinemage) {
   for (let master in kinemage.masterDict) {
-    var shape = new NGL.Shape(master, {
-      pointSize: 3,
-      sizeAttenuation: false,
-      lineWidth: 3
-    })
+    var shape = new NGL.Shape(master)
 
     kinemage.dotLists.forEach(function (dotList) {
-      if (!dotList.master.includes(master)) return
-      for (var i = 0, il = dotList.position.length / 3; i < il; ++i) {
-        var i3 = i * 3
-        var x = dotList.position[ i3 ]
-        var y = dotList.position[ i3 + 1 ]
-        var z = dotList.position[ i3 + 2 ]
-        var r = dotList.color[ i3 ]
-        var g = dotList.color[ i3 + 1 ]
-        var b = dotList.color[ i3 + 2 ]
-        shape.addPoint([ x, y, z ], [ r, g, b ], dotList.label[ i ])
-      }
+      if (!dotList.masterArray.includes(master)) return
+      var pointBuffer = new NGL.PointBuffer({
+        position: new Float32Array(dotList.positionArray),
+        color: new Float32Array(dotList.colorArray)
+      }, {
+        pointSize: 2,
+        sizeAttenuation: false,
+        useTexture: true
+      })
+      shape.addBuffer(pointBuffer)
     })
 
     kinemage.vectorLists.forEach(function (vectorList) {
-      if (!vectorList.master.includes(master)) return
-      for (var i = 0, il = vectorList.position1.length / 3; i < il; ++i) {
-        var i3 = i * 3
-        var x1 = vectorList.position1[ i3 ]
-        var y1 = vectorList.position1[ i3 + 1 ]
-        var z1 = vectorList.position1[ i3 + 2 ]
-        var x2 = vectorList.position2[ i3 ]
-        var y2 = vectorList.position2[ i3 + 1 ]
-        var z2 = vectorList.position2[ i3 + 2 ]
-        var r = vectorList.color1[ i3 ]
-        var g = vectorList.color1[ i3 + 1 ]
-        var b = vectorList.color1[ i3 + 2 ]
-        shape.addWideline([ x1, y1, z1 ], [ x2, y2, z2 ], [ r, g, b ], vectorList.label1[ i ])
-      }
+      if (!vectorList.masterArray.includes(master)) return
+      var widelineBuffer = new NGL.WidelineBuffer({
+        position1: new Float32Array(vectorList.position1Array),
+        position2: new Float32Array(vectorList.position2Array),
+        color: new Float32Array(vectorList.color1Array),
+        color2: new Float32Array(vectorList.color2Array)
+      }, {
+        linewidth: vectorList.width[0]
+      })
+      shape.addBuffer(widelineBuffer)
     })
+
+    kinemage.ballLists.forEach(function (ballList) {
+      if (!ballList.masterArray.includes(master)) return
+      var sphereBuff = new NGL.SphereBuffer({
+        position: new Float32Array(ballList.positionArray),
+        radius: new Float32Array(ballList.radiusArray),
+        color: new Float32Array(ballList.colorArray)
+      }, {
+        useTexture: true
+      })
+      shape.addBuffer(sphereBuff)
+    })
+
+    var positionArray = []
+    var colorArray = []
+    kinemage.ribbonLists.forEach(function (ribbonList) {
+      if (!ribbonList.masterArray.includes(master)) return
+      positionArray.push(...ribbonList.positionArray)
+      colorArray.push(...ribbonList.colorArray)
+    })
+    if (positionArray.length > 0) {
+      var meshBuffer = new NGL.MeshBuffer({
+        position: new Float32Array(positionArray),
+        color: new Float32Array(colorArray)
+      }, {
+        side: 'double'
+      })
+      shape.addBuffer(meshBuffer)
+    }
 
     var visible = kinemage.masterDict[ master ].visible
     var shapeComp = stage.addComponentFromObject(shape, { visible: visible })
