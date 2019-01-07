429fa5b498148f74a1a9b7a6acefbf4fb9cdc764
Alexander Rose <alex.rose@rcsb.org>
2019-01-07 15:27:38 -0800

    Revert "fixed `expandBoundingBoxFactor` ui param"
    This reverts commit acb44840f588e350af77523be83eb019afbbf856.

--- src/ui/parameters.ts
+++ src/ui/parameters.ts
@@ -57,6 +57,6 @@ export const UIStageParameters: { [k in keyof StageParameters]: ParamType } = {
   ambientIntensity: NumberParam(2, 10, 0),
   hoverTimeout: IntegerParam(10000, -1),
   tooltip: BooleanParam(),
-  expandBoundingBoxFactor: NumberParam(1, 10, 1),
+  expandBoundingBoxFactor: NumberParam(1, 1, 100),
   mousePreset: SelectParam(...Object.keys(MouseActionPresets))
 }
