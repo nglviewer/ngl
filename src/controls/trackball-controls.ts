13949a950f0aee8d759b65c081a53413c4c5b21b
Gary Oberbrunner <garyo@darkstarsystems.com>
2020-07-10 16:03:08 -0400

    Make trackball controls work with camera not just on -Z axis
    Have to account for camera rotation so that trackball controls stay in
    screen X/Y.

--- src/controls/trackball-controls.ts
+++ src/controls/trackball-controls.ts
@@ -18,8 +18,10 @@ const tmpRotateXMatrix = new Matrix4()
 const tmpRotateYMatrix = new Matrix4()
 const tmpRotateZMatrix = new Matrix4()
 const tmpRotateMatrix = new Matrix4()
+const tmpRotateCameraMatrix = new Matrix4()
 const tmpRotateVector = new Vector3()
 const tmpRotateQuaternion = new Quaternion()
+const tmpRotateQuaternion2 = new Quaternion()
 const tmpPanMatrix = new Matrix4()
 const tmpPanVector = new Vector3()
 const tmpAtomVector = new Vector3()
@@ -73,12 +75,24 @@ class TrackballControls {
     ]
   }
 
+  private _getCameraRotation(m: Matrix4) {
+    m.extractRotation(this.viewer.camera.matrixWorld)
+    m.multiply(tmpRotateYMatrix.makeRotationY(Math.PI))
+
+    return m
+  }
+
   private _transformPanVector () {
     if (!this.component) return
 
+    // Adjust for component and scene rotation
     tmpPanMatrix.extractRotation(this.component.transform)
     tmpPanMatrix.premultiply(this.viewer.rotationGroup.matrix)
     tmpPanMatrix.getInverse(tmpPanMatrix)
+
+    // Adjust for camera rotation
+    tmpPanMatrix.multiply(this._getCameraRotation(tmpRotateMatrix))
+
     tmpPanVector.applyMatrix4(tmpPanMatrix)
   }
 
@@ -89,7 +103,12 @@ class TrackballControls {
   pan (x: number, y: number) {
     this._setPanVector(x, y)
 
+    // Adjust for scene rotation
     tmpPanMatrix.getInverse(this.viewer.rotationGroup.matrix)
+
+    // Adjust for camera rotation
+    tmpPanMatrix.multiply(this._getCameraRotation(tmpRotateMatrix))
+
     tmpPanVector.applyMatrix4(tmpPanMatrix)
     this.controls.translate(tmpPanVector)
   }
@@ -121,10 +140,19 @@ class TrackballControls {
   rotate (x: number, y: number) {
     const [ dx, dy ] = this._getRotateXY(x, y)
 
-    tmpRotateXMatrix.makeRotationX(dy)
-    tmpRotateYMatrix.makeRotationY(dx)
-    tmpRotateXMatrix.multiply(tmpRotateYMatrix)
-    this.controls.applyMatrix(tmpRotateXMatrix)
+    // rotate around screen X then screen Y
+    this._getCameraRotation(tmpRotateMatrix)
+    tmpRotateVector.set(1, 0, 0) // X axis
+    tmpRotateVector.applyMatrix4(tmpRotateMatrix) // screen X
+    tmpRotateQuaternion.setFromAxisAngle(tmpRotateVector, dy)
+
+    tmpRotateVector.set(0, 1, 0) // Y axis
+    tmpRotateVector.applyMatrix4(tmpRotateMatrix) // screen Y
+    tmpRotateQuaternion2.setFromAxisAngle(tmpRotateVector, dx)
+
+    tmpRotateQuaternion.multiply(tmpRotateQuaternion2)
+    tmpRotateMatrix.makeRotationFromQuaternion(tmpRotateQuaternion)
+    this.controls.applyMatrix(tmpRotateMatrix)
   }
 
   zRotate (x: number, y: number) {
@@ -139,18 +167,25 @@ class TrackballControls {
 
     const [ dx, dy ] = this._getRotateXY(x, y)
 
+    this._getCameraRotation(tmpRotateCameraMatrix)
+
     tmpRotateMatrix.extractRotation(this.component.transform)
     tmpRotateMatrix.premultiply(this.viewer.rotationGroup.matrix)
     tmpRotateMatrix.getInverse(tmpRotateMatrix)
+    tmpRotateMatrix.premultiply(tmpRotateCameraMatrix)
+
     tmpRotateVector.set(1, 0, 0)
     tmpRotateVector.applyMatrix4(tmpRotateMatrix)
     tmpRotateXMatrix.makeRotationAxis(tmpRotateVector, dy)
+
     tmpRotateVector.set(0, 1, 0)
     tmpRotateVector.applyMatrix4(tmpRotateMatrix)
     tmpRotateYMatrix.makeRotationAxis(tmpRotateVector, dx)
+
     tmpRotateXMatrix.multiply(tmpRotateYMatrix)
     tmpRotateQuaternion.setFromRotationMatrix(tmpRotateXMatrix)
     this.component.quaternion.premultiply(tmpRotateQuaternion)
+    this.component.quaternion.normalize()
     this.component.updateMatrix()
   }
 }
