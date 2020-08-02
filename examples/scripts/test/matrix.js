9a2fdc36e8475bd6c477c1ff6f6f3412b7e7d9a2
Fred Ludlow <fred.ludlow@astx.com>
2020-08-02 09:31:19 +0100

    Update test example to include map boxSize

--- examples/scripts/test/matrix.js
+++ examples/scripts/test/matrix.js
@@ -1,8 +1,8 @@
-stage.loadFile('data://1blu.pdb').then(function (o) {
+const a = [-0.017893094929114478, 0.44789033770201286, 0.8939094375533996, 0, 0.9995957623640608, -0.011742974659610292, 0.025892362000326047, 0, 0.022094094633404514, 0.8940113802068294, -0.4474991654104058, 0, 14.428066535570798, 7.843669892047377, 7.242749062873044, 1]
+
+stage.loadFile('data://1lee.pdb').then(function (o) {
   // Check correct behaviour of picking/representation etc when
   // applying a transformation matrix to a component.
-  const a = [-0.017893094929114478, 0.44789033770201286, 0.8939094375533996, 0, 0.9995957623640608, -0.011742974659610292, 0.025892362000326047, 0, 0.022094094633404514, 0.8940113802068294, -0.4474991654104058, 0, 14.428066535570798, 7.843669892047377, 7.242749062873044, 1]
-
   const m = new NGL.Matrix4()
   m.fromArray(a)
 
@@ -37,4 +37,17 @@ stage.loadFile('data://1blu.pdb').then(function (o) {
 
   console.log(a)
   console.log(m)
+})
+
+stage.loadFile('data://1lee.ccp4').then(function (o) {
+  const m = new NGL.Matrix4()
+  m.fromArray(a)
+
+  o.setTransform(m)
+  o.addRepresentation('surface', {
+    contour: true,
+    color: 'skyblue',
+    boxSize: 10,
+    useWorker: false
+  })
 })
