f95ca469c83d47ede70b7f6205add397d3a69ae6
Fred Ludlow <fred.ludlow@gmail.com>
2020-09-23 21:24:29 +0100

    Fix color of orbital in test/negative-iso

--- examples/scripts/test/negative-iso.js
+++ examples/scripts/test/negative-iso.js
@@ -5,5 +5,5 @@ stage.loadFile('data://water.pdb').then(function (o) {
 
 stage.loadFile('data://water.cube').then(function (o) {
   o.addRepresentation('surface', {opacity: 0.5, colorValue: 'cyan', disablePicking: true})
-  o.addRepresentation('surface', {negateIsolevel: true, opacity: 0.5, colorValue: 'green', disablePicking: true})
+  o.addRepresentation('surface', {negateIsolevel: true, opacity: 0.5, colorValue: 'red', disablePicking: true})
 })
