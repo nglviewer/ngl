c8c4ae50069fcb5ec2e357893153d01ca0da0c25
Danny Diaz <danny.jesus.diaz.94@gmail.com>
2020-11-07 11:40:52 -0600

    Refactored compvis-viewer.js to improve readability.

--- examples/scripts/interactive/compvis-viewer.js
+++ examples/scripts/interactive/compvis-viewer.js
@@ -1,6 +1,6 @@
 Promise.all([
-  stage.loadFile('rcsb://6ij6.pdb'),
-  NGL.autoLoad('data://6ij6.csv', {
+  stage.loadFile('rcsb://6qgb.pdb'),
+  NGL.autoLoad('data://6qgb.csv', {
     ext: 'csv',
     delimiter: ' ',
     comment: '#',
@@ -16,15 +16,30 @@ Promise.all([
   var schemeId = NGL.ColormakerRegistry.addScheme(function (params) {
     this.atomColor = function (atom) {
       for (var i = 0; i <= csv.length; i++) {
-        if (atom.resno === parseFloat(csv[i][4])) {
-          if (parseFloat(csv[i][7]) < 0.05) {
+
+
+        let csv_res_num_col = 4;
+        let csv_wt_prob_col = 7;
+
+        let wt_prob         = parseFloat(csv[i][csv_wt_prob_col]);
+        let res_num         = parseFloat(csv[i][csv_res_num_col]);
+
+
+        if (atom.resno === res_num) {
+          if (wt_prob < 0.01) {
             return 0xFF0080// hot pink
-          } else if (parseFloat(csv[i][7]) < 0.10) {
+          } else if (parseFloat(csv[i][7] < 0.05)) {
             return 0xFF0000 // red
-          } else {
+          } else if (wt_prob < 0.10) {
+            return 0xFFA500
+          } else if (wt_prob < 0.25) {
+            return 0xFFFF00 // yellow
+          }
+          else {
             return 0xFFFFFF // white
           }
         } else {
+          //TODO what do we do here? We need to know what the implications of else
           continue
         }
       }
