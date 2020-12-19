9436b6699b6b8d8ecb1d5667a9f8d186becc6128
Brad Alexander <bralexander@live.com>
2020-12-18 18:33:20 -0600

    fixed calculations for last bug

--- examples/scripts/interactive/residueClickLigand.js
+++ examples/scripts/interactive/residueClickLigand.js
@@ -66,7 +66,7 @@ stage.signals.hovered.add(function (pickingProxy) {
       // var mp = pickingProxy.mouse.position
       // console.log('pick', pickingProxy.atom.resno)
       var index = atom.resno - firstResNum
-      if (index < csv.length && index > firstResNum) {
+      if (index < csv.length && atom.resno >= firstResNum) {
         tooltip.innerHTML = `
       RESNO: ${atom.resno}<br/>
       WT AA: ${atom.resname}<br/>
