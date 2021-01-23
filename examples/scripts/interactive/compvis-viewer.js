c22d541a05fd6ea6789002eaf67d5a87a7225fec
Brad Alexander <bralexander@live.com>
2021-01-23 15:44:21 -0800

    merged from pull-req2 branch and updated data sources to machineLearning

--- examples/scripts/interactive/compvis-viewer.js
+++ examples/scripts/interactive/compvis-viewer.js
@@ -13,6 +13,16 @@ function createElement (name, properties, style) {
   return el
 }
 
+function createSelect (options, properties, style) {
+  var select = createElement('select', properties, style)
+  options.forEach(function (d) {
+    select.add(createElement('option', {
+      value: d[0], text: d[1]
+    }))
+  })
+  return select
+}
+
 function createFileButton (label, properties, style) {
   var input = createElement('input', Object.assign({
     type: 'file'
@@ -53,85 +63,43 @@ stage.signals.hovered.add(function (pickingProxy) {
   if (cartoonCheckbox.checked === true || customCheckbox.checked === true) {
     if (pickingProxy && (pickingProxy.atom || pickingProxy.bond)) {
       var atom = pickingProxy.atom || pickingProxy.closestBondAtom
-      // var mp = pickingProxy.mouse.position
-      // console.log('pick', pickingProxy.atom.resno)
-      var index = atom.resno - firstResNum
-      if (index < csv.length && atom.resno >= firstResNum) {
+      const csvRow = residueData[atom.resno]
+      if (csvRow !== undefined) {
         tooltip.innerHTML = `
       RESNO: ${atom.resno}<br/>
       WT AA: ${atom.resname}<br/>
-      WT PROB: ${csv[index][csvWtProbCol]}<br/>
-      PRED AA: ${csv[index][csvPrAaCol]}<br/>
-      PRED PROB: ${csv[index][csvPrProbCol]}<br/>`
+      WT PROB: ${csvRow[csvWtProbCol]}<br/>
+      PRED AA: ${csvRow[csvPrAaCol]}<br/>
+      PRED PROB: ${csvRow[csvPrProbCol]}<br/>`
         tooltip.style.bottom = 3 + 'px'
         tooltip.style.left = stage.viewer.width - 200 + 'px'
         tooltip.style.display = 'block'
-      } else {
+      } 
+      else {
         tooltip.style.display = 'none'
       }
     }
   }
 })
 
-function getGradientColor (startColor, endColor, thirdColor, percent) {
-  // switch for second gradient i.e white to red for heat map
-  if (percent >= 1) {
-    percent -= 1
-    startColor = endColor
-    endColor = thirdColor
-  }
-
-  // get colors
-  var startRed = parseInt(startColor.substr(0, 2), 16)
-  var startGreen = parseInt(startColor.substr(2, 2), 16)
-  var startBlue = parseInt(startColor.substr(4, 2), 16)
-
-  var endRed = parseInt(endColor.substr(0, 2), 16)
-  var endGreen = parseInt(endColor.substr(2, 2), 16)
-  var endBlue = parseInt(endColor.substr(4, 2), 16)
-
-  // calculate new color
-  var diffRed = endRed - startRed
-  var diffGreen = endGreen - startGreen
-  var diffBlue = endBlue - startBlue
-
-  diffRed = ((diffRed * percent) + startRed).toString(16).split('.')[0]
-  diffGreen = ((diffGreen * percent) + startGreen).toString(16).split('.')[0]
-  diffBlue = ((diffBlue * percent) + startBlue).toString(16).split('.')[0]
-
-  // ensure 2 digits by color (necessary)
-  if (diffRed.length === 1) diffRed = '0' + diffRed
-  if (diffGreen.length === 1) diffGreen = '0' + diffGreen
-  if (diffBlue.length === 1) diffBlue = '0' + diffBlue
-
-  return '0x' + diffRed + diffGreen + diffBlue
-}
-
-function makeGradientArray () {
-  var gradientArray = []
-  for (var count = 0; count < 101; count++) {
-    var newColor = getGradientColor('FF0000', 'FFFFFF', '0000FF', (0.02 * count))
-    var numColor = parseInt(Number(newColor), 10)
-    gradientArray.push(numColor)
-  }
-  return gradientArray
-}
-
-var gradientArray = makeGradientArray()
 
 var ligandSele = '( not polymer or not ( protein or nucleic ) ) and not ( water or ACE or NH2 )'
 
 var pocketRadius = 0
 var pocketRadiusClipFactor = 1
 
-var cartoonRepr, neighborRepr, ligandRepr, contactRepr, pocketRepr, labelRepr, customRepr
+var cartoonRepr, spacefillRepr, neighborRepr, ligandRepr, contactRepr, pocketRepr, labelRepr, customRepr
 
 var heatMap, customPercent
 
 var struc
 var csv
+var residueData
+
 var neighborSele
 var sidechainAttached = false
+
+var loadStrucFile, loadCsvFile
 
 const csvResNumCol = 4
 const csvWtProbCol = 7
@@ -139,11 +107,13 @@ const csvPrAaCol = 6
 const csvPrProbCol = 8
 
 var firstResNum
+var resNum, wtProb, normWtProb
 
 function loadStructure (proteinFile, csvFile) {
   struc = undefined
   stage.setFocus(0)
   stage.removeAllComponents()
+  ligandSelect.innerHTML = ''
   clipNearRange.value = 0
   clipRadiusRange.value = 100
   pocketOpacityRange.value = 0
@@ -160,10 +130,10 @@ function loadStructure (proteinFile, csvFile) {
   cationPiCheckbox.checked = true
   piStackingCheckbox.checked = true
   return Promise.all([
-    stage.loadFile(proteinFile, { defaultRepresentation: true }),
+    stage.loadFile(proteinFile),
     NGL.autoLoad(csvFile, {
       ext: 'csv',
-      delimiter: ',',
+      delimiter: ' ',
       comment: '#',
       columnNames: true
     })
@@ -171,17 +141,17 @@ function loadStructure (proteinFile, csvFile) {
     struc = ol[0]
     csv = ol[1].data
 
-    var residueData = {}
-    console.log('l', csv.length)
+    firstResNum = parseInt(csv[0][csvResNumCol])
+
+    setLigandOptions()
+    setChainOptions()
+    setResidueOptions()
+
+    residueData = {}
     for (var i = 0; i < csv.length; i++) {
-      const index = parseFloat(csv[i][0])
       const resNum = parseFloat(csv[i][csvResNumCol])
-      console.log(i, resNum)
-      // 228 is undefined (2isk)
       residueData[resNum] = csv[i]
     }
-
-    firstResNum = parseInt(csv[0][csvResNumCol])
 
     var heatMap = NGL.ColormakerRegistry.addScheme(function (params) {
       this.domain = [ 0.5, 1 ]
@@ -189,58 +159,31 @@ function loadStructure (proteinFile, csvFile) {
       this.mode = 'rgb'
       var scale = this.getScale()
       this.atomColor = function (atom) {
-        //console.log(atom.residueIndex)
-        // var index = parseFloat(residueData[atom.residueIndex][0])
-        // console.log(atom.residueIndex, index)
-        //console.log('max', atom.resno[0])
         const csvRow = residueData[atom.resno]
-        // const resNum = parseFloat(csvRow[0])
         if (atom.isNucleic()) {
           return 0x004e00
         }
         if (csvRow !== undefined) {
           const wtProb = parseFloat(csvRow[csvWtProbCol])
           return scale(wtProb)
-        //var value = parseFloat(csv[ atom.resno ][ csvWtProbCol ])
         }  else {
-        return 0xFF0080
+        return 0xcccccc
       }
     }
     })
-
-    // heatMap = NGL.ColormakerRegistry.addScheme(function (params) {
-    //   this.atomColor = function (atom) {
-    //     /* old for loop */
-    //     // for (var i = 0; i < csv.length; i++) {
-    //     //   // console.log(i)
-    //     //   // ^ causing infinite loop
-    //     //   const wtProb = parseFloat(csv[i][csvWtProbCol])
-    //     //   const resNum = parseFloat(csv[i][csvResNumCol])
-    //     //   const normWtProb = (wtProb * 100).toFixed(0)
-
-    //     /* uses residueData obj*/
-    //     const csvRow = residueData[atom.residueIndex]
-    //     const wtProb = parseFloat(csvRow[csvWtProbCol])
-    //     const resNum = parseFloat(csvRow[csvResNumCol])
-
-    //     const normWtProb = (wtProb * 100).toFixed(0)
-
-    //       if (atom.resno === resNum) {
-    //         return gradientArray[normWtProb]
-    //       }
-    //     // }
-    //   }
-    // })
 
     customPercent = NGL.ColormakerRegistry.addScheme(function (params) {
       this.atomColor = function (atom) {
         for (var i = 0; i < csv.length; i++) {
           const predProb = parseFloat(csv[i][csvPrProbCol])
           const wtProb = parseFloat(csv[i][csvWtProbCol])
+          // console.log('wt', wtProb)
           const resNum = parseFloat(csv[i][csvResNumCol])
 
           if (atom.resno === resNum) {
-            if (wtProb < 0.01 && predProb > 0.7) {
+            if (atom.isNucleic()) {
+              return 0x004e00
+            } else if (wtProb < 0.01 && predProb > 0.7) {
               return 0xFF0080// hot pink
             } else if (wtProb < 0.01) {
               return 0xCC00FF // hot pink
@@ -267,6 +210,10 @@ function loadStructure (proteinFile, csvFile) {
       color: customPercent,
       visible: false
     })
+    spacefillRepr = struc.addRepresentation('spacefill', {
+      sele: ligandSele,
+      visible: false
+    })
     neighborRepr = struc.addRepresentation('ball+stick', {
       sele: 'none',
       aspectRatio: 1.1,
@@ -277,7 +224,7 @@ function loadStructure (proteinFile, csvFile) {
       multipleBond: 'symmetric',
       colorValue: 'grey',
       sele: 'none',
-      aspectRatio: 1.1,
+      aspectRatio: 1.2,
       radiusScale: 2.5
     })
     contactRepr = struc.addRepresentation('contact', {
@@ -314,34 +261,93 @@ function loadStructure (proteinFile, csvFile) {
       labelGrouping: 'residue'
     })
   })
+  .catch(failure)
 }
 
-var instructionsText = createElement('span', {
-  innerHTML: `
-  This tool is for rendering Machine Learning data on proteins. <br/>
-  To use with your own Machine Learning CSV: <br/>
-  Copy the column order of data/machineLearning/2isk.csv in the source code<br/>
-  Then load your local structure and csv files.
-  `
-}, { top: getTopPosition(), left: '12px', color: 'grey' })
-addElement(instructionsText)
+// ERROR HANDLING for input box
+function failure (error) {
+  console.error(error)
+  if (window.confirm('Sorry, this data does not exist.')) {
+  }
+}
 
-var loadStrucFile, loadCsvFile
-var loadStructureButton = createFileButton('Load Structure 1st', {
+function setLigandOptions () {
+  ligandSelect.innerHTML = ''
+  var options = [['', 'select ligand']]
+  struc.structure.eachResidue(function (rp) {
+    if (rp.isWater()) return
+    var sele = ''
+    if (rp.resno !== undefined) sele += rp.resno
+    if (rp.inscode) sele += '^' + rp.inscode
+    if (rp.chain) sele += ':' + rp.chainname
+    var name = (rp.resname ? '[' + rp.resname + ']' : '') + sele
+    if (rp.entity && rp.entity.description) name += ' (' + rp.entity.description + ')'
+    options.push([sele, name])
+  }, new NGL.Selection(ligandSele))
+  options.forEach(function (d) {
+    ligandSelect.add(createElement('option', {
+      value: d[0], text: d[1]
+    }))
+  })
+}
+
+function setChainOptions () {
+  chainSelect.innerHTML = ''
+  var options = [['', 'select chain']]
+  struc.structure.eachChain(function (cp) {
+    var name = cp.chainname
+    if (cp.entity && cp.entity.description) name += ' (' + cp.entity.description + ')'
+    options.push([cp.chainname, name])
+  }, new NGL.Selection('polymer'))
+  options.forEach(function (d) {
+    chainSelect.add(createElement('option', {
+      value: d[0], text: d[1]
+    }))
+  })
+}
+
+function setResidueOptions (chain) {
+  residueSelect.innerHTML = ''
+  var options = [['', 'select residue']]
+  if (chain) {
+    struc.structure.eachResidue(function (rp) {
+      var sele = ''
+      if (rp.resno !== undefined) sele += rp.resno
+      if (rp.inscode) sele += '^' + rp.inscode
+      if (rp.chain) sele += ':' + rp.chainname
+      var name = (rp.resname ? '[' + rp.resname + ']' : '') + sele
+      options.push([sele, name])
+    }, new NGL.Selection('polymer and :' + chain))
+  }
+  options.forEach(function (d) {
+    residueSelect.add(createElement('option', {
+      value: d[0], text: d[1]
+    }))
+  })
+}
+
+var loadStructureButton = createFileButton('Load Structure', {
   accept: '.pdb,.cif,.ent,.gz,.mol2',
   onchange: function (e) {
     if (e.target.files[0]) {
       loadStrucFile = e.target.files[0]
     }
+    if (loadCsvFile) {
+      loadStructure(loadStrucFile, loadCsvFile)
+      loadCsvFile = ''
+      loadStrucFile = ''
+    }
   }
-}, { top: getTopPosition(70), left: '12px' })
+}, { top: getTopPosition(), left: '12px' })
 addElement(loadStructureButton)
 
-var loadCsvButton = createFileButton('Load csv 2nd', {
+var loadCsvButton = createFileButton('Load csv', {
   accept: '.csv',
   onchange: function (e) {
     if (e.target.files[0]) {
       loadCsvFile = e.target.files[0]
+    }
+    if (loadStrucFile) {
       loadStructure(loadStrucFile, loadCsvFile)
       loadCsvFile = ''
       loadStrucFile = ''
@@ -350,7 +356,26 @@ var loadCsvButton = createFileButton('Load csv 2nd', {
 }, { top: getTopPosition(20), left: '12px' })
 addElement(loadCsvButton)
 
+var loadPdbidInput = createElement('input', {
+  type: 'text',
+  placeholder: 'Enter pdbID, Try:',
+  onkeypress: function (e) {
+    if (e.keyCode === 13) {
+      var inputValue = e.target.value.toLowerCase()
+      var proteinInput = 'data://machineLearning/' + inputValue + '.pdb'
+      var csvInput = 'data://machineLearning/' + inputValue + '.csv'
+      e.preventDefault()
+      loadStructure(proteinInput, csvInput)
+    }
+  }
+}, { top: getTopPosition(20), left: '12px', width: '120px' })
+addElement(loadPdbidInput)
+
 function showFull () {
+  ligandSelect.value = ''
+
+  spacefillRepr.setVisibility(false)
+
   ligandRepr.setVisibility(false)
   neighborRepr.setVisibility(false)
   contactRepr.setVisibility(false)
@@ -373,6 +398,8 @@ function showLigand (sele) {
   pocketRadius = Math.max(sview.boundingBox.getSize(new NGL.Vector3()).length() / 2, 2) + 10
   var withinSele2 = s.getAtomSetWithinSelection(new NGL.Selection(sele), pocketRadius + 2)
   var neighborSele2 = '(' + withinSele2.toSeleString() + ') and not (' + sele + ') and polymer'
+
+  spacefillRepr.setVisibility(false)
 
   ligandRepr.setVisibility(true)
   neighborRepr.setVisibility(true)
@@ -397,12 +424,15 @@ function showLigand (sele) {
 
 function showRegion (sele) {
   var s = struc.structure
+  ligandSele.value = ''
 
   var withinSele = s.getAtomSetWithinSelection(new NGL.Selection(sele), 5)
   var withinGroup = s.getAtomSetWithinGroup(withinSele)
   var expandedSele = withinGroup.toSeleString()
   neighborSele = '(' + expandedSele + ') and not (' + sele + ')'
   neighborSele = expandedSele
+
+  spacefillRepr.setVisibility(false)
 
   ligandRepr.setVisibility(false)
   neighborRepr.setVisibility(false)
@@ -413,7 +443,42 @@ function showRegion (sele) {
   struc.autoView(expandedSele, 2000)
 }
 
-// onclick residue select and show ligand
+var ligandSelect = createSelect([], {
+  onchange: function (e) {
+    residueSelect.value = ''
+    var sele = e.target.value
+    if (!sele) {
+      showFull()
+    } else {
+      showLigand(sele)
+    }
+  }
+}, { top: getTopPosition(30), left: '12px', width: '130px' })
+addElement(ligandSelect)
+
+var chainSelect = createSelect([], {
+  onchange: function (e) {
+    ligandSelect.value = ''
+    residueSelect.value = ''
+    setResidueOptions(e.target.value)
+  }
+}, { top: getTopPosition(20), left: '12px', width: '130px' })
+addElement(chainSelect)
+
+var residueSelect = createSelect([], {
+  onchange: function (e) {
+    ligandSelect.value = ''
+    var sele = e.target.value
+    if (!sele) {
+      showFull()
+    } else {
+      showLigand(sele)
+    }
+  }
+}, { top: getTopPosition(20), left: '12px', width: '130px' })
+addElement(residueSelect)
+
+// onclick residue select and show ligandrepr
 var prevSele = ''
 stage.signals.clicked.add(function (pickingProxy) {
   if (pickingProxy === undefined) {
@@ -446,7 +511,7 @@ stage.signals.clicked.add(function (pickingProxy) {
 
 addElement(createElement('span', {
   innerText: 'pocket near clipping'
-}, { top: getTopPosition(20), left: '12px', color: 'grey' }))
+}, { top: getTopPosition(30), left: '12px', color: 'grey' }))
 var clipNearRange = createElement('input', {
   type: 'range', value: 0, min: 0, max: 10000, step: 1
 }, { top: getTopPosition(16), left: '12px' })
@@ -510,6 +575,21 @@ var customCheckbox = createElement('input', {
 addElement(customCheckbox)
 addElement(createElement('span', {
   innerText: 'Custom'
+}, { top: getTopPosition(), left: '32px', color: 'grey' }))
+
+var sidechainAttachedCheckbox = createElement('input', {
+  type: 'checkbox',
+  checked: false,
+  onchange: function (e) {
+    sidechainAttached = e.target.checked
+    neighborRepr.setSelection(
+      sidechainAttached ? '(' + neighborSele + ') and (sidechainAttached or not polymer)' : neighborSele
+    )
+  }
+}, { top: getTopPosition(20), left: '12px' })
+addElement(sidechainAttachedCheckbox)
+addElement(createElement('span', {
+  innerText: 'sidechainAttached'
 }, { top: getTopPosition(), left: '32px', color: 'grey' }))
 
 var labelCheckbox = createElement('input', {
