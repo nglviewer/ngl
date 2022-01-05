41483f9c398ef4cf8b32815e7d39f83e77888fac
Andy Widdess <awiddess@ccdc.cam.ac.uk>
2022-01-05 09:33:43 +0000

    VIS-48 Resolve issues with dependencies and resolving getInverse() deprecation

--- src/align/superposition.ts
+++ src/align/superposition.ts
@@ -217,9 +217,7 @@ class Superposition {
         })
 
         //update transformation matrices for each assembly
-
-        const invertTrasform = new Matrix4()
-        invertTrasform.getInverse(transform)
+        const invertTrasform = new Matrix4().copy(transform).invert();
 
         const biomolDict = atoms.biomolDict
 
