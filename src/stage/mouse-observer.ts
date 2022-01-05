604fd0e61dd1508d8b111be5c91e154682483e44
Jonathan Romano <jonathan@luxaritas.com>
2022-01-05 13:17:21 -0500

    Fix hovering behavior when using synthetic events in FireFox

--- src/stage/mouse-observer.ts
+++ src/stage/mouse-observer.ts
@@ -456,13 +456,13 @@ class MouseObserver {
 
   _setCanvasPosition (event: any) {  // TODO
     const box = this.domElement.getBoundingClientRect()
-    let offsetX, offsetY
-    if ('offsetX' in event && 'offsetY' in event) {
+    let offsetX, offsetY;
+    if ('clientX' in event && 'clientY' in event) {
+      offsetX = event.clientX - left
+      offsetY = event.clientY - top
+    } else {
       offsetX = event.offsetX
       offsetY = event.offsetY
-    } else {
-      offsetX = event.clientX - box.left
-      offsetY = event.clientY - box.top
     }
     this.canvasPosition.set(offsetX, box.height - offsetY)
   }
