189ac879fd9b8755ee7c9ba84053932202b5f674
Seungwoo Hong <qksn1541@gmail.com>
2023-07-02 02:31:57 +0900

    remove: buble compiler

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import json from '@rollup/plugin-json'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
-import buble from '@rollup/plugin-buble'
 import internal from 'rollup-plugin-internal'
 
 // import terser from 'rollup-plugin-terser'
--- src/polyfills.js
+++ src/polyfills.js
@@ -1,3 +1,5 @@
+// TODO remove..?
+
 /**
  * @file shims
  * @private
