54363c1d46479e92fe812f135522b84984e2dabf
ppillot <paulpillot@gmail.com>
2023-12-23 15:34:14 -0500

    Typings: colormaker registry

--- src/color/colormaker.ts
+++ src/color/colormaker.ts
@@ -54,6 +54,7 @@ function SRGBToLinear( c: number ) {
   return ( c < 0.04045 ) ? c * 0.0773993808 : Math.pow( c * 0.9478672986 + 0.0521327014, 2.4 );
 }
 
+export type ColormakerConstructor = new (...p: ConstructorParameters<typeof Colormaker>) => Colormaker
 
 /**
  * Class for making colors.
